da3c14e6db7fa3c0a066bce94528ae72c0cae330
sefatanam <sefatanam@gmail.com>
2024-04-06 13:58:16 +0600

    Revert "refactor: transform function into pipe"
    This reverts commit 7e409126451bf3116c987e77c4b4eead3609114f.

--- apps/angular/pipe-easy/src/app/app.component.ts
+++ apps/angular/pipe-easy/src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { HeavyComputationPipe } from './heavy-computation.pipe';
 
 @Component({
   standalone: true,
-  imports: [NgFor, HeavyComputationPipe],
+  imports: [NgFor],
   selector: 'app-root',
   template: `
     <div *ngFor="let person of persons; let index = index">
-      {{ person | heavyComputation: index }}
+      {{ heavyComputation(person, index) }}
     </div>
   `,
 })
 export class AppComponent {
   persons = ['toto', 'jack'];
+
+  heavyComputation(name: string, index: number) {
+    // very heavy computation
+    return `${name} - ${index}`;
+  }
 }
